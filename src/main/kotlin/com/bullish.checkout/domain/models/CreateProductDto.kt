8f4ac65388a8d8ac6f3fcc64d104c5eb4311637b
Izen Huang <izenhuang@hotmailcom>
2022-11-18 16:04:40 +0800

    refactor: move DTOs to new directory

--- src/main/kotlin/com/bullish.checkout/domain/models/CreateProductDto.kt
+++
@@ -1,7 +0,0 @@
-package com.bullish.checkout.domain.models
-
-data class CreateProductDto(
-    val name: String,
-    val description: String?,
-    val price: Double
-)
--- src/main/kotlin/com/bullish.checkout/domain/models/UpdateBasketDto.kt
+++
@@ -1,5 +0,0 @@
-package com.bullish.checkout.domain.models
-
-data class UpdateBasketDto(
-    val products: Map<String, Int>
-)
